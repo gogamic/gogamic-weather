11c5702946ba2fe1234a08f0d00385b0407bbb2b
gogamic <gamicsocial@gmail.com>
2020-09-08 21:04:46 +0530

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./style.css";
 import { DatePicker } from 'antd';
 import 'antd/dist/antd.css';
-import Navbat from './Nav.js';
+import Navbar from './Nav.js';
+import Searchbar from './Searchbox.js';
 
 
 export default function App() {
 
   return (
     <>
-   
-</>
+    <Navbar />
+    <center>
+    <Searchbar />
+    </center>
+    </>
   );
 }
---
+++ src/Searchbox.js
@@ -0,0 +1,29 @@
+import { Input } from 'antd';
+import React from "react";
+import { AudioOutlined } from '@ant-design/icons';
+
+const { Search } = Input;
+
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1890ff',
+    }}
+  />
+);
+
+export default function Searchbar() {
+  return(
+  <>
+    <Search
+      placeholder="Search for a Place"
+      onSearch={value => console.log(value)}
+      style={{ width: 200 }}
+    />
+  <br />
+   
+  </>
+ 
+  );
+  }
