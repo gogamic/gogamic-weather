04c0061754a91c0ae38983d1bfaa711c8e28c698
gogamic <gamicsocial@gmail.com>
2020-09-11 12:27:39 +0530

    1 file modified

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,5 @@
-import { Menu, Button } from 'antd';
-import {
-  AppstoreOutlined,
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  PieChartOutlined,
-  DesktopOutlined,
-  ContainerOutlined,
-  MailOutlined,
-  ContactsOutlined,
-  HomeOutlined, 
-  GithubOutlined ,
-} from '@ant-design/icons';
+import { Menu } from 'antd';
+import {HomeOutlined, GithubOutlined , ContactsOutlined  } from '@ant-design/icons';
 import React from "react";
 import {Animated} from "react-animated-css";
 
@@ -19,44 +8,32 @@ const { SubMenu } = Menu;
 
 export default class Navbar extends React.Component {
   state = {
-    collapsed: false,
+    current: 'mail',
   };
 
-  toggleCollapsed = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({ current: e.key });
   };
 
   render() {
+    const { current } = this.state;
     return (
-      <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
-      <div style={{ width: 256 }}>
-        <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
-          {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
-        </Button>
-        <Menu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          mode="inline"
-          theme="dark"
-          inlineCollapsed={this.state.collapsed}
-        >
-          <Menu.Item key="1" icon={<HomeOutlined />}>
-           Home
-          </Menu.Item>
-          <Menu.Item key="2" icon={<GithubOutlined />}>
-           Github
-          </Menu.Item>
-          <Menu.Item key="3" icon={<ContactsOutlined />}>
-           Contact
-          </Menu.Item>
-        
-        </Menu>
-      </div>
-      </Animated>
-
+      <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key="mail" icon={<HomeOutlined />}>
+          Home
+        </Menu.Item>
+        <Menu.Item key="github" icon={<GithubOutlined />}>
+          Github
+        </Menu.Item>
+        <Menu.Item key="contact" icon={<ContactsOutlined />}>
+         Contact Us
+        </Menu.Item>
+       
+       
+      </Menu>
     );
+  
   }
 }
 
