967f3adaf99749c236e730434b53dac60ae71ea5
gogamic <gamicsocial@gmail.com>
2020-09-15 18:41:35 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default function App() {
   return (
   <Router>
       <div>
-      
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <Navbar />
         <br />
         
